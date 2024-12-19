d5f8ba38249c14ddc17594d4a901de51b8518ff1
subaru-hello <octosubaru0926@gmail.com>
2024-12-19 21:57:43 +0900

    fix: imageを表示

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,13 @@
-import { Text, View, StyleSheet } from "react-native";
-import { Link } from "expo-router";
+import { View, StyleSheet } from "react-native";
 
+import { Image } from "expo-image";
+import PlaceHolderImage from "@/assets/images/background-image.png";
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home screen</Text>
-      <Link href="/about" style={styles.button}>
-        Go to About screen
-      </Link>
+      <View style={styles.image}>
+        <Image source={PlaceHolderImage} style={styles.image} />
+      </View>
     </View>
   );
 }
@@ -17,14 +17,10 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#25292e",
     alignItems: "center",
-    justifyContent: "center",
   },
-  text: {
-    color: "#fff",
-  },
-  button: {
-    fontSize: 20,
-    textDecorationLine: "underline",
-    color: "#fff",
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
   },
 });
