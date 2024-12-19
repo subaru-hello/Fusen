02880f9c9928feb3db887251bf0a3620323396a3
subaru-hello <octosubaru0926@gmail.com>
2024-12-19 22:08:03 +0900

    fix: build screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,19 @@
 import { View, StyleSheet } from "react-native";
 
-import { Image } from "expo-image";
-import PlaceHolderImage from "@/assets/images/background-image.png";
+import Button from "@/components/Button";
+import ImageViewer from "@/components/ImageViewer";
+
+const PlaceholderImage = require("@/assets/images/background-image.png");
+
 export default function Index() {
   return (
     <View style={styles.container}>
-      <View style={styles.image}>
-        <Image source={PlaceHolderImage} style={styles.image} />
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="Choose a photo" />
+        <Button label="Use this photo" />
       </View>
     </View>
   );
@@ -18,9 +25,11 @@ const styles = StyleSheet.create({
     backgroundColor: "#25292e",
     alignItems: "center",
   },
-  image: {
-    width: 320,
-    height: 440,
-    borderRadius: 18,
+  imageContainer: {
+    flex: 1,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: "center",
   },
 });
